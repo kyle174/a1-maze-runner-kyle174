0ba06eb39ad23c5297e823e77e03ee16a51d8d98
kyle174 <kyoojojo@gmail.com>
2024-01-14 14:18:35 -0500

    added input flag when file is loaded

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -11,26 +11,31 @@ public class Main {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
-        try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        System.out.print("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        System.out.print("PASS ");
+        if (args[0].equals("-i") || args[0].equals("--input")) {
+            logger.info("** Starting Maze Runner");
+            try {
+                logger.info("**** Reading the maze from file " + args[1]);
+                BufferedReader reader = new BufferedReader(new FileReader(args[1]));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    for (int idx = 0; idx < line.length(); idx++) {
+                        if (line.charAt(idx) == '#') {
+                            System.out.print("WALL ");
+                        } else if (line.charAt(idx) == ' ') {
+                            System.out.print("PASS ");
+                        }
                     }
+                    System.out.print(System.lineSeparator());
                 }
-                System.out.print(System.lineSeparator());
+            } catch(Exception e) {
+                logger.error("/!\\ An error has occurred /!\\");
             }
-        } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            logger.info("**** Computing path");
+            logger.error("PATH NOT COMPUTED");
+            logger.info("** End of MazeRunner");
         }
-        System.out.println("**** Computing path");
-        System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
+        else {
+            logger.error("MISSING INPUT FLAG");
+        }
     }
 }
